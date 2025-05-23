115f7e7b55533e08cf0245af14a7bf290299e4ed
yamanuk <hunnyescape69@gmail.com>
2025-05-23 12:11:04 +0900

    push

--- src/app/components/floating-images-fix/FloatingImagesFixSection.tsx
+++ src/app/components/floating-images-fix/FloatingImagesFixSection.tsx
@@ -21,7 +21,9 @@ const FloatingImagesFixSection: React.FC = () => {
 				/>
 				<div
 					className="absolute inset-0 z-10 block sm:hidden bg-center bg-cover"
-					style={{ backgroundImage: "url('/images/garally_small.png')" }}
+					style={{
+						backgroundImage: `url(${process.env.NEXT_PUBLIC_CLOUDFRONT_URL}/pepe/garally_small.webp)`
+					}}
 				/>
 
 				<FloatingImagesFixCanvas />

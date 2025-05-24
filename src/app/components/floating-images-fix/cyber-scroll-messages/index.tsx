00d906111a51bdd98c058f36e56f98bf5e816d06
yamanuk <hunnyescape69@gmail.com>
2025-05-24 16:18:35 +0900

    after delete

--- src/app/components/floating-images-fix/cyber-scroll-messages/index.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/components/floating-images-fix/cyber-scroll-messages/index.tsx
-
-'use client';
-
-import CyberScrollMessages from './CyberScrollMessages';
-
-// 明示的にデフォルトエクスポート
-export default CyberScrollMessages;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 import HeroSection from './components/hero-section/HeroSection';
-import SphereTop from './components/sphere/SphereTop';
-import PepeTop from './components/pepe3d/PepeTop';
 import GlowingTextSection from './components/glowing-3d-text/GlowingTextSection';
-import PulsatingComponent from './components/layout/PulsatingComponent';
-import FloatingImagesFixSection from './components/floating-images-fix/FloatingImagesFixSection';
 import Header from './components/ui/Header';
 import Footer from './components/ui/Footer';
 import CyberInterface from './components/layout/CyberInterface';
 import PepePush from './components/pepePush/PepePush';
-import LayeredGallerySection from './components/layered-gallery/LayeredGallerySection'
 export default function Home() {
 	return (
 		<main className="relative flex flex-col items-center w-full">
@@ -21,18 +16,3 @@ export default function Home() {
 		</main>
 	);
 }
-
-/*
-			<Header />
-			<HeroSection />
-			<CyberInterface />
-			<GlowingTextSection />
-			<PulsatingComponent />
-			<PepeTop />
-			<SphereTop />
-			<PepePush />
-			<Footer />
-
-			<LayeredGallerySection /
-
-*/
